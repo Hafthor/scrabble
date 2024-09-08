7123e08580ab13b01a59990310c76c2c1ad4dc52
Hafthor <git@hafthor.net>
2024-09-08 01:45:35 -0400

    nits

--- Scrabble/Program.cs
+++ Scrabble/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace Scrabble;
 
-public class Program {
-    static void Main(string[] args) {
+public static class Program {
+    public static void Main(string[] args) {
         int players = args.Length > 0 ? int.Parse(args[0]) : 2;
         int seed = args.Length > 1 ? int.Parse(args[1]) : new Random().Next();
         Random random = new(seed);
@@ -10,16 +10,16 @@ public class Program {
         int passes = 0;
         while (passes < players) {
             game.Board.Print();
-            var curplayer = game.Players[game.Turn];
-            Console.Write($"Player {game.Turn + 1} ({curplayer.Score}) tiles: ");
-            Console.WriteLine(string.Join(" ", curplayer.Tiles));
+            var curPlayer = game.Players[game.Turn];
+            Console.Write($"Player {game.Turn + 1} ({curPlayer.Score}) tiles: ");
+            Console.WriteLine(string.Join(" ", curPlayer.Tiles));
             string cmd;
             for (;;) {
                 Console.Write("Enter row,col,h/v,word or help: ");
                 cmd = Console.ReadLine();
                 if (cmd is "") {
                     var plays = game.Board.GetPossiblePlays(wordList.WordsByLen, wordList.LetterCountsForWordsByLen,
-                            curplayer.Tiles)
+                            curPlayer.Tiles)
                         .OrderBy(p => p.score + p.extras.Sum(e => e.score)).ThenBy(p => p.word);
                     foreach (var (y, x, h, w, score, extras, underlinePositions) in plays) {
                         string uw = Underline(w, underlinePositions);
@@ -43,26 +43,26 @@ public class Program {
                 }
                 passes = 0;
                 if (cmd == "rack") {
-                    game.Bag.AddRange(curplayer.Tiles);
-                    curplayer.Tiles.Clear();
+                    game.Bag.AddRange(curPlayer.Tiles);
+                    curPlayer.Tiles.Clear();
                     game.ShuffleBag(random);
-                    curplayer.Draw(game.Bag);
+                    curPlayer.Draw(game.Bag);
                     game.Turn = (game.Turn + 1) % players;
                     break;
                 }
                 if (cmd is null or "exit" or "quit") break;
-                string[] parts = cmd.Split(',');
-                int row = int.Parse(parts[0]) - 1, col = int.Parse(parts[1]) - 1;
-                bool horizontal = parts[2].ToUpper() == "H";
-                string word = parts[3].ToUpper();
+                string[] cmdParts = cmd.Split(',');
+                int row = int.Parse(cmdParts[0]) - 1, col = int.Parse(cmdParts[1]) - 1;
+                bool horizontal = cmdParts[2].ToUpper() == "H";
+                string word = cmdParts[3].ToUpper();
                 if (!wordList.WordsByLen[word.Length].Contains(word)) {
                     Console.WriteLine("Invalid word");
                     continue;
                 }
-                int currentPlayerTurn = game.Turn, currentScore = game.Players[currentPlayerTurn].Score;
+                int currentPlayerTurn = game.Turn, currentScore = curPlayer.Score;
                 string error = game.Play(word, row, col, horizontal, wordList.WordsByLen);
                 if (error == null) {
-                    int newScore = game.Players[currentPlayerTurn].Score;
+                    int newScore = curPlayer.Score;
                     Console.WriteLine(
                         $"Player {currentPlayerTurn + 1} adds {newScore - currentScore} to score, new score = {newScore}");
                     break;
@@ -74,9 +74,10 @@ public class Program {
         game.Board.Print();
         Console.WriteLine("Game over");
         int maxScore = game.Players.Max(p => p.Score);
-        for (int i = 0; i < players; i++)
-            Console.WriteLine($"Player {i + 1}: {game.Players[i].Score}" +
-                              (game.Players[i].Score == maxScore ? " (winner)" : ""));
+        for (int i = 0; i < players; i++) {
+            var p = game.Players[i];
+            Console.WriteLine($"Player {i + 1}: {p.Score}" + (p.Score == maxScore ? " (winner)" : ""));
+        }
     }
 
     private static string Underline(string word, int positions) {
